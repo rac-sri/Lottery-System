eb7998bfc6516d84a46d92e61a928081b2110ba2
rachit2501 <rac.sri25@gmail.com>
2020-03-19 15:32:05 +0530

    completed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,82 @@ import React, { Component } from "react";
 import web3 from "./web3";
 import "./index.css";
 import ReactDOM from "react-dom";
+import lottery from "./lottery";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      manager: "",
+      players: [],
+      balance: "",
+      value: "",
+      message: ""
+    };
+  }
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
+  }
+
+  onSubmit = async event => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success..." });
+    console.log("sadflkdsk");
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether")
+    });
+    console.log("dsajkfnsd");
+    this.setState({ message: "You have been entered" });
+  };
+
+  onClick = async event => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: "Waiting on transaction success..." });
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({ message: "A winner has been picked!" });
+  };
   render() {
-    return <h1>App</h1>;
+    return (
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>
+          this contract is managed but {this.state.manager}. Thre are currently{" "}
+          {this.state.players.length} people entered competing to wine{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")}
+        </p>
+
+        <hr />
+
+        <form onSubmit={this.onSubmit}>
+          <h4>Try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+
+        <hr />
+        <h4>Ready to picj a winner?</h4>
+        <button onClick={this.onClick}>Pick a Winner!</button>
+        <hr />
+        <h1>{this.state.message}</h1>
+      </div>
+    );
   }
 }
 
--- src/lottery.js
+++ src/lottery.js
@@ -1,3 +1,59 @@
 import web3 from "./web3";
 
-const address = "0xd8416ff7719aC95ef1add2478636EcFbFf171155";
+const address = "0xeC28dD614b373945Cd846374A67E5CCB4a476403";
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
